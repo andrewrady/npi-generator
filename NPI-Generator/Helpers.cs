d14842d5ddf1538b7b2fdb86bd62062815040d09
Andrew Rady <andrew.rady@ritterim.com>
2022-12-21 14:29:32 -0700

    init

---
+++ NPI-Generator/Helpers.cs
@@ -0,0 +1,51 @@
+﻿namespace NPI_Generator;
+
+public abstract class Helpers
+{
+    public static InputValidation ValidateInput(string? input)
+    {
+        if (string.IsNullOrEmpty(input)) return new InputValidation { IsValid = false };
+        var isValid = int.TryParse(input, out int result);
+        if (isValid == false)
+        {
+            return new InputValidation
+            {
+                IsValid = isValid,
+                Message = $"{input} is not a valid input",
+                ParsedAmount = result
+            };
+        }
+
+        return new InputValidation
+        {
+            IsValid = isValid,
+            Message = "Valid",
+            ParsedAmount = result
+        };
+    }
+
+    public static IList<string> GenerateNpi(int amount)
+    {
+        IList<string> npiList = new List<string>();
+        for(var i = 0; i < amount; i++)
+        {
+            var randomNpi = GenerateNpi();
+            npiList.Add(randomNpi);
+        }
+
+        return npiList;
+    }
+    
+    private static string GenerateNpi()
+    {
+        var ran = new Random(10);
+        return ran.Next().ToString();
+    }
+
+    public class InputValidation
+    {
+        public bool IsValid { get; set; }
+        public string Message { get; set; }
+        public int ParsedAmount { get; set; }
+    }
+}
---
+++ NPI-Generator/Program.cs
@@ -0,0 +1,14 @@
+﻿using NPI_Generator;
+const string initialQuestion = "How many NPI do you want?";
+
+Console.WriteLine(initialQuestion);
+var amount = Console.ReadLine();
+var result = Helpers.ValidateInput(amount);
+
+if (result.IsValid)
+{
+    var npiList = Helpers.GenerateNpi(result.ParsedAmount);
+    Console.WriteLine(string.Join("\n", npiList));
+    return;
+}
+Console.WriteLine(result.Message);
